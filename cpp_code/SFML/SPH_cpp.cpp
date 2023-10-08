dc608d1c541e5089806d3846dbb0135ac6e63e7d
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-10-08 12:54:41 -0500

    added Makefile

---
+++ cpp_code/SFML/SPH_cpp.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>	// To threads in SFML
+using namespace std;
+
+
+
+int main()
+	{
+		// Create the main window
+		sf::RenderWindow App(sf::VideoMode(800, 600, 32), "SFML Window");
+		
+	}
